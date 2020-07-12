b6e17325f7ccac84ed31073e80507436aa6e1065
nielsgal <galjaardniels@gmail.com>
2020-07-12 23:20:13 +0200

    bug fix and logging

--- server1/main.go
+++ server1/main.go
@@ -1,22 +1,28 @@
 package main
 
 import(
+	"fmt"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type User{
+type User struct{
 	gorm.Model
-	Email string `json"Email"`
+	Email string `json:"Email"`
 }
+var (
+	DBConn *gorm.DB
+)
 
 func InitDatabase(){
-	DBConn, err = gorm.Open("postgres","host=database port=5432 user=niels dbname=test_database password=galjaard")
+	var err error
+	DBConn, err = gorm.Open("postgres","host=localhost port=5432 user=niels dbname=test_database password=galjaard sslmode=disable")
 	if err != nil {
+		fmt.Println(err)
 		panic("failed to connect to database")
 	}
-	defer DBConn.close()
+	defer DBConn.Close()
 	DBConn.AutoMigrate(&User{})
 }
 
@@ -24,4 +30,5 @@ func InitDatabase(){
 func main() {
 	app := fiber.New()
 	InitDatabase()
+	app.Listen(3000)
 }
